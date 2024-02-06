5b5c51334481ce7605b5389a46619715b1f6c0a3
Mark McCaskey <maoeurk@gmail.com>
2024-02-06 20:40:28 +0800

    Tweak effect

--- game.gd
+++ game.gd
@@ -26,10 +26,16 @@ func _on_boss_boss_died() -> void:
 var camera_shake_intensity: float = 10.0
 
 func player_damage_taken() -> void:
-	var extent = ((100 - max(player.health, 0)) / 10)
+	var extent = ((100 - max(player.health, 0)) / 15)
 	chromatic_aberation_effect.material.set_shader_parameter("r_displacement", Vector2(extent, extent / 3))
 	chromatic_aberation_effect.material.set_shader_parameter("b_displacement", Vector2(extent * -1, extent / -3))
 	for i in range(6):
-		camera.offset.x = randf() * camera_shake_intensity - camera_shake_intensity / 2
-		camera.offset.y = randf() * camera_shake_intensity - camera_shake_intensity / 2
+		var x_val = randf() * camera_shake_intensity - camera_shake_intensity / 2
+		var y_val = randf() * camera_shake_intensity - camera_shake_intensity / 2
+		camera.offset.x = x_val
+		camera.offset.y = y_val
+		chromatic_aberation_effect.material.set_shader_parameter("r_displacement", Vector2(extent * y_val, (extent * x_val) / 3))
+		chromatic_aberation_effect.material.set_shader_parameter("b_displacement", Vector2(extent * y_val * -1, (extent * x_val) / -3))
 		await get_tree().create_timer(0.05).timeout
+	chromatic_aberation_effect.material.set_shader_parameter("r_displacement", Vector2(extent, extent / 3))
+	chromatic_aberation_effect.material.set_shader_parameter("b_displacement", Vector2(extent * -1, extent / -3))
